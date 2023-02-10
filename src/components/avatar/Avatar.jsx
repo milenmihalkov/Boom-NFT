08b195efdcef4482c2fffa00614eb5c13f055a14
Milen <milenmihalkov@gmail.com>
2023-02-10 15:05:05 +0200

    Avatar componet
    Set default value of verified to false

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles  from "./Avatar.module.scss";
 
 
-export default function Avatar ({ size = 90, url = "/images/avatar.png", verified = true }) {
+export default function Avatar ({ size = 90, url = "/images/avatar.png", verified = false }) {
 
     return (
       <div className={styles.avatar} style={{ width: size, height: size }}>
