b63f0705efdc9c27c248855a959598d20c28bd04
Milen <milenmihalkov@gmail.com>
2023-02-10 17:00:24 +0200

    Avatar

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,20 +1,4 @@
-// import React from 'react';
-// import classNames from "classnames";
-// import style from "./Avatar.module.scss";
-
-
-// export default function Avatar ({ size = 90, url, verified = false }) {
-
-//     return (
-//       <div style={{ width: size, height: size }} className={classNames(style.avatar)} >
-//         <img src={url} className={classNames(style.image)} />
-//         {verified && (
-//           <img src='/images/verified.svg' className={classNames(style.badge)} />
-//         )}
-//       </div>
-//     );
-//   };
-
+import React from 'react';
 import classNames from "classnames";
 import styles from "./Avatar.module.scss";
 
