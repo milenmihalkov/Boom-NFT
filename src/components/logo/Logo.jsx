593a328f1fcb18f4dd3b6c725bfc6494fe1c39f4
Milen <milenmihalkov@gmail.com>
2023-02-10 17:35:02 +0200

    345435

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,40 +1,40 @@
-export default function Logo({ type = "default" }) {
-
-
-  const logoDefault = <img src="/images/logo.svg" alt="logo" />;
-  const logoMuted = <img src="/images/logo-muted.svg" alt="muted logo" />;
-
-  let logo;
-
-  if (!type || type == "default") {
-    logo = logoDefault;
-  } else if (type == "muted") {
-    logo = logoMuted;
-  }
-
-  return logo;
-}
-// import { useState, useEffect } from "react";
-
-
 // export default function Logo({ type = "default" }) {
 
 
-//   const [logoType, setLogoType] = useState(type)
+//   const logoDefault = <img src="/images/logo.svg" alt="logo" />;
+//   const logoMuted = <img src="/images/logo-muted.svg" alt="muted logo" />;
 
-//   const path ={
-//     default : "/images/logo.svg",
-//     muted   : "/images/logo-muted.svg"
+//   let logo;
+
+//   if (!type || type == "default") {
+//     logo = logoDefault;
+//   } else if (type == "muted") {
+//     logo = logoMuted;
 //   }
 
-//   useEffect(() => {
-//     setLogoType(type);
-//   }, [type]);
-
-//   return (
-//     <img
-//     src={path[logoType]} 
-//     alt="Logo"
-//     />
-//   )
+//   return logo;
 // }
+import { useState, useEffect } from "react";
+
+
+export default function Logo({ type = "default" }) {
+
+
+  const [logoType, setLogoType] = useState(type)
+
+  const path ={
+    default : "/images/logo.svg",
+    muted   : "/images/logo-muted.svg"
+  }
+
+  useEffect(() => {
+    setLogoType(type);
+  }, [type]);
+
+  return (
+    <img
+    src={path[logoType]} 
+    alt="Logo"
+    />
+  )
+}
