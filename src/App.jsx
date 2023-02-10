2821ac58e4e491ac72cded2ab3291807f6520fb2
Milen <milenmihalkov@gmail.com>
2023-02-10 10:11:56 +0200

    create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 // import Paper from "@mui/material/Paper";
 // import Container from "@mui/material/Container";
 // import Link from "../link/Link";
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Logo from "./logo/Logo";
+import Logo from "../logo/Logo";
 
 const Header = ()=>{
     return(
 
     <header>
         <Logo/>
-        
     </header>
     )
 }
