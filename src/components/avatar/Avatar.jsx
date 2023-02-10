e778fba37aefcd19635c2798ae3d0164fa0babef
Milen <milenmihalkov@gmail.com>
2023-02-10 16:15:10 +0200

    Avatar remove image

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 import style from "./Avatar.module.scss";
 
 
-export default function Avatar ({ size = 90, url = "/images/avatar.png", verified = false }) {
+export default function Avatar ({ size = 90, url, verified = false }) {
 
     return (
       <div className={classNames(style.avatar)} style={{ width: size, height: size }}>
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import classNames  from "classnames";
 import style from "./User.module.scss";
 import Avatar from "../avatar/Avatar";
 
 
-export default function User({name="", info="", avatar="", size="55", verified="false"}) {
+export default function User({name="", info="", avatar="/images/avatar.png", size="55", verified=false}) {
 
     return(
-        <div className={style.user}>
-            <Avatar size={size}/>
-            <div className="name">{name}</div>
-            <div className="info">{info}</div>
+        <div className={classNames(style.user)}>
+            <Avatar size={size} verified={verified} url={avatar}/>
+            <div>
+                <div className={classNames(style.name)}>{name}</div>
+                <div className={classNames(style.info)}>{info}</div>
+            </div>
         </div>
     )
     
