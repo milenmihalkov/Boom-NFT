63a4996b4e60795a121ad96601ce1e2198c9deaa
Milen <milenmihalkov@gmail.com>
2023-02-10 18:34:33 +0200

    User
    size prop must be in number

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,7 +4,7 @@ import styles from "./User.module.scss";
 import Avatar from "../avatar/Avatar";
 
 
-export default function User({name="", info="", avatar="", size="55", verified=false}) {
+export default function User({name="", info="", avatar="", size=55, verified=false}) {
 
     return(
         <div className={classNames(styles.user)}>
