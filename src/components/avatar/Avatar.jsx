a658002a71094dce9bd74b3a768a5e3baecd9822
Milen <milenmihalkov@gmail.com>
2023-02-10 16:37:28 +0200

    Avatar

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+// import React from 'react';
+// import classNames from "classnames";
+// import style from "./Avatar.module.scss";
+
+
+// export default function Avatar ({ size = 90, url, verified = false }) {
+
+//     return (
+//       <div style={{ width: size, height: size }} className={classNames(style.avatar)} >
+//         <img src={url} className={classNames(style.image)} />
+//         {verified && (
+//           <img src='/images/verified.svg' className={classNames(style.badge)} />
+//         )}
+//       </div>
+//     );
+//   };
+
 import classNames from "classnames";
-import style from "./Avatar.module.scss";
+import styles from "./Avatar.module.scss";
 
-
-export default function Avatar ({ size = 90, url, verified = false }) {
-
-    return (
-      <div style={{ width: size, height: size }} className={classNames(style.avatar)} >
-        <img src={url} className={classNames(style.image)} />
-        {verified && (
-          <img src='/images/verified.svg' className={classNames(style.badge)} />
-        )}
-      </div>
-    );
-  };
+export default function Avatar({ size = 90, verified = false, url }) {
+  return (
+    <div
+      style={{ width: size , height: size}}
+      className={classNames(styles.avatar)}
+    >
+      <img src={url} alt="" className={classNames(styles.image)} />
+      {verified && (
+        <img src="/images/verified.svg" className={classNames(styles.badge)} />
+      )}
+    </div>
+  );
+}
