b514447175b44fc784a32d36d6db4b8856f9197e
Milen <milenmihalkov@gmail.com>
2023-02-20 12:07:15 +0200

    Try Top Collector

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,6 @@
 import App from "../src/App";
+import { React, useState, useEffect } from "react";
+
 import Header from "../src/components/header/Header";
 import Featured from "../src/components/featured/Featured";
 import Trending from "../src/components/trending/Trending";
@@ -7,20 +9,35 @@ import How from "../src/components/how/How";
 import Auctions from "../src/components/auctions/Auctions";
 import Footer from "../src/components/footer/Footer";
 
+
 import dataFeatured from "../data/featured.json";
 import dataTrending from "../data/trending.json";
 import dataUsers from "../data/users.json";
 import dataNfts from "../data/nfts.json";
 
 export default function Index() {
+
+  const [featuredCards, setFeaturedCards] = useState([]);
+  const [trendingCards, setTrendingCards] = useState([]);
+  const [topCollectors, setTopCollectors] = useState([]);
+  const [auctionNfts, setAuctionNfts] = useState([]);
+
+  useEffect(() => {
+    setFeaturedCards(dataFeatured.slice(0, 6));
+    setTrendingCards(dataTrending);
+    setTopCollectors(dataUsers.slice(0, 12));
+    setAuctionNfts(dataNfts);
+  }, [dataFeatured, dataTrending, dataUsers, dataNfts]);
+
+  console.log(featuredCards);
   return (
     <>
       <Header/>
-      <Featured items={dataFeatured}/>
-      <Trending cards={dataTrending}/>
-      <TopCollectors/>
+      <Featured items={featuredCards} />
+      <Trending cards={trendingCards} />
+      <TopCollectors collectors={topCollectors}/>
       <How/>
-      <Auctions/>
+      <Auctions cards={auctionNfts}/>
       <Footer/>
     </>
 
--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -4,10 +4,29 @@ import Step from './Step';
 import styles from './How.module.scss';
 
 export default function How({
-    description="Discover, collect, and sell extraordinary NFTs on the world's first & largest NFT marketplace. There are  three things you'll need in place to open your account and start buying or selling NFTs on BUM.",
-    title="HOW IT WORKS",
-    items = [],
-    link
+    description = "Discover, collect, and sell extraordinary NFTs on the world's first & largest NFT marketplace. There are  three things you'll need in place to open your account and start buying or selling NFTs on BUM.",
+  title = "HOW IT WORKS",
+  items = [
+    {
+      title: "Digital Currency",
+      number: 1,
+      description:
+        "You can get ETH, the digital currency that fuels transactions on the Ethereum blockchain, from a digital currency exchange",
+    },
+    {
+      title: "Crypto Wallet",
+      number: 2,
+      description:
+        "A crypto wallet, such as MetaMask, stores your ETH and processes transactions on the Ethereum blockchain.",
+    },
+    {
+      title: "BUM.",
+      number: 3,
+      description:
+        "Let's connect your wallet to BUM, edit your profile, and begin interacting in the space. ",
+    },
+  ],
+  link,
 
 }) {
     return (
