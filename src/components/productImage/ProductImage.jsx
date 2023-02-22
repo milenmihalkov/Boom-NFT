0a88fca143e111d99114f83ca4787d6c84a98255
Milen <milenmihalkov@gmail.com>
2023-02-22 11:49:54 +0200

    Productimage

---
+++ src/components/productImage/ProductImage.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styles from "./ProductImage.module.scss";
+
+export default function ProductImage({ url }) {
+  return (
+    <div className={styles.product-image}>
+      <img className={styles.image} src={url} alt=""></img>
+    </div>
+  );
+}
